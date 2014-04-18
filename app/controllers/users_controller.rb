e309ae4907a3522f9e0d10ec5dd88befde6c230a
Li Donghua <donghua.li@dianping.com>
2014-04-19 01:03:33 +0800

    Add missing file.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,7 @@ class UsersController < ApplicationController
       redirect_to users_url
     end
   end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password,
